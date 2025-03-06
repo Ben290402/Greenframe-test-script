67c9c14ddfbfd5b6f7ffda877adf4e87867a5ca7
Ben290402 <71718194+Ben290402@users.noreply.github.com>
2025-03-06 16:25:49 +0100

    Update tests.spec.ts

--- tests.spec.ts
+++ tests.spec.ts
@@ -2,10 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('access api A', async ({ page }) => {
     await page.goto('http://host.docker.internal:81/api-a');
-    await page.getByRole('button').click();
 });
 
 test('access api B', async ({ page }) => {
     await page.goto('http://host.docker.internal:81/api-b');
-    await page.getByRole('button').click();
 });
