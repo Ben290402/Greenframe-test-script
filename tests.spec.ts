0bdc74fd9338aa1d779ebeadaa1748a2380bea79
Ben290402 <71718194+Ben290402@users.noreply.github.com>
2025-03-06 16:29:10 +0100

    Update tests.spec.ts

--- tests.spec.ts
+++ tests.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
 test('access api A', async ({ page }) => {
     await page.goto('http://host.docker.internal:81/api-a');
