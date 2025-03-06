4e8a5bef986f8d78d243ca81acaba5823af6c650
Ben290402 <71718194+Ben290402@users.noreply.github.com>
2025-03-06 16:46:36 +0100

    Update tests.spec.ts

--- tests.spec.ts
+++ tests.spec.ts
@@ -1,9 +1,10 @@
-const { test, expect } = require('@playwright/test');
+const scenario = async (page) => {
+    await page.goto('http://host.docker.internal:81/api-a', { waitUntil: 'networkidle' });
+    await page.addMilestone('Accès à l\'API A');
+    await page.waitForTimeout(1000); // Attendre 1 seconde entre les accès
+    await page.goto('http://host.docker.internal:81/api-b', { waitUntil: 'networkidle' });
+    await page.addMilestone('Accès à l\'API B');
+    await page.waitForNetworkIdle(); // S'assurer que toutes les requêtes sont terminées
+};
 
-test('access api A', async ({ page }) => {
-    await page.goto('http://host.docker.internal:81/api-a');
-});
-
-test('access api B', async ({ page }) => {
-    await page.goto('http://host.docker.internal:81/api-b');
-});
+module.exports = scenario;
